97bfe354420f02b092d087ae9dfc32daa4ee9831
Jimmy Lord <jimmylord@gmail.com>
2015-04-23 08:41:37 -0400

    Two way blur for testing multiple post effects.

---
+++ GameEmptyReplaceMe/Game/Data/Shaders/PostEffects/BlurVertical.glsl
@@ -0,0 +1,48 @@
+#ifdef WIN32
+#define lowp
+#define mediump
+#else
+precision mediump float;
+#endif
+
+varying vec2 v_UVCoord;
+
+#ifdef VertexShader
+
+attribute vec4 a_Position;
+attribute vec2 a_UVCoord;
+
+uniform mat4 u_WorldViewProj;
+
+void main()
+{
+    gl_Position = a_Position;
+	v_UVCoord = a_UVCoord;
+}
+
+#endif
+
+#ifdef FragmentShader
+
+uniform sampler2D u_TextureColor;
+
+void main()
+{
+	vec4 color = vec4(0); //texture2D( u_TextureColor, v_UVCoord );
+
+    float offset = 2.0/1024.0;
+
+	color += texture2D( u_TextureColor, vec2( v_UVCoord.x, v_UVCoord.y - offset*4.0 ) ) * 0.05;
+	color += texture2D( u_TextureColor, vec2( v_UVCoord.x, v_UVCoord.y - offset*3.0 ) ) * 0.09;
+	color += texture2D( u_TextureColor, vec2( v_UVCoord.x, v_UVCoord.y - offset*2.0 ) ) * 0.13;
+	color += texture2D( u_TextureColor, vec2( v_UVCoord.x, v_UVCoord.y - offset     ) ) * 0.15;
+	color += texture2D( u_TextureColor, v_UVCoord                                     ) * 0.16;
+	color += texture2D( u_TextureColor, vec2( v_UVCoord.x, v_UVCoord.y + offset     ) ) * 0.15;
+	color += texture2D( u_TextureColor, vec2( v_UVCoord.x, v_UVCoord.y + offset*2.0 ) ) * 0.13;
+	color += texture2D( u_TextureColor, vec2( v_UVCoord.x, v_UVCoord.y + offset*3.0 ) ) * 0.09;
+	color += texture2D( u_TextureColor, vec2( v_UVCoord.x, v_UVCoord.y + offset*4.0 ) ) * 0.05;
+
+	gl_FragColor = color;
+}
+
+#endif
