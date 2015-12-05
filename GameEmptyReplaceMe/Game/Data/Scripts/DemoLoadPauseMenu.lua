dd1d63e911d450bc4665428812b5165710f9cf64
Jimmy Lord <jimmylord@gmail.com>
2015-12-05 11:11:15 -0500

    Started adding a pause menu to demo scenes.

---
+++ GameEmptyReplaceMe/Game/Data/Scripts/DemoLoadPauseMenu.lua
@@ -0,0 +1,25 @@
+-- DemoLoadPauseMenu Script
+
+DemoLoadPauseMenu =
+{
+
+OnLoad = function()
+	g_pEngineCore:LoadSceneFromFile( "Data/Scenes/DemoPauseMenu.scene" );
+end,
+
+OnPlay = function()
+end,
+
+OnStop = function()
+end,
+
+OnTouch = function(action, id, x, y, pressure, size)
+end,
+
+OnButtons = function(action, id)
+end,
+
+Tick = function(timepassed)
+end
+
+}
