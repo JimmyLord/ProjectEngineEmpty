eb179a7d0930ec9386825277cdfee6c4bb38ee5d
Jimmy Lord <jimmylord@gmail.com>
2016-07-11 10:52:03 -0400

    VoxelWorldCamera now locks mouse.

--- GameEmptyReplaceMe/Game/Data/Voxels/VoxelWorldCamera.lua
+++ GameEmptyReplaceMe/Game/Data/Voxels/VoxelWorldCamera.lua
@@ -11,9 +11,6 @@ Externs =
 };
 
 OnLoad = function()
-	this.lastmousex = -1;
-	this.lastmousey = -1;
-
 	this.mousedirx = 0;
 	this.mousediry = 0;
 
@@ -22,24 +19,23 @@ OnLoad = function()
 end,
 
 OnPlay = function()
+	SetMouseLock( true );
 end,
 
 OnStop = function()
+	SetMouseLock( false );
 end,
 
 OnTouch = function(action, id, x, y, pressure, size)
 
-	--LogInfo( "OnTouch " .. this.mousedirx .. "\n" );
+	if( action == 0 ) then --GCBA_Down
+		SetMouseLock( true );
+	end
 
-	this.mousedirx = (x - this.lastmousex) * -1;
-	this.mousediry = (y - this.lastmousey) * 1;
-
-	--this.lastmousex = x;
-	--this.lastmousey = y;
-
-	SetMousePosition( 640/2, 960/2 );
-	this.lastmousex = 640/2;
-	this.lastmousey = 960/2;
+	if( action == 2 ) then --GCBA_Held
+		this.mousedirx = x * -1;
+		this.mousediry = y * -1;
+	end
 
 end,
 
@@ -49,8 +45,8 @@ end,
 Tick = function(timepassed)
 
 	-- Spin camera based on mouse x/y
-	this.anglex = this.anglex + this.mousediry / 10;
-	this.angley = this.angley + this.mousedirx / 10;
+	this.anglex = this.anglex + this.mousediry * 1;
+	this.angley = this.angley + this.mousedirx * 1;
 
 	this.mousedirx = 0;
 	this.mousediry = 0;
