d772274a3a7d1b0436052b7e2fdb31035001870b
Jimmy Lord <jimmylord@gmail.com>
2017-05-22 09:30:56 -0400

    Updated shaders to expect vec3s for light color.

--- GameEmptyReplaceMe/Game/Data/Shaders/Shader_TextureLights.glsl
+++ GameEmptyReplaceMe/Game/Data/Shaders/Shader_TextureLights.glsl
@@ -73,25 +73,25 @@ precision mediump float;
         float shadowperc = CalculateShadowPercentage();
 
         // Hardcoded ambient
-        vec4 finalambient = vec4(0.2, 0.2, 0.2, 1.0);
+        vec3 finalambient = vec3( 0.2, 0.2, 0.2 );
 
         // Accumulate diffuse and specular color for all lights.
-        vec4 finaldiffuse = vec4(0,0,0,0);
-        vec4 finalspecular = vec4(0,0,0,0);
+        vec3 finaldiffuse = vec3(0,0,0);
+        vec3 finalspecular = vec3(0,0,0);
             
         // Add in each light, one by one. // finaldiffuse, finalspecular are inout.
 #if NUM_LIGHTS > 0
         for( int i=0; i<NUM_LIGHTS; i++ )
-            PointLightContribution( i, v_WSPosition, u_WSCameraPos, WSnormal, u_Shininess, finaldiffuse, finalspecular );
+            PointLightContribution( i, v_WSPosition.xyz, u_WSCameraPos, WSnormal, u_Shininess, finaldiffuse, finalspecular );
 #endif
 
         // Mix the texture color with the light color.
-        vec4 ambdiff = texcolor * v_Color * ( finalambient + finaldiffuse );
-        vec4 spec = u_TextureSpecColor * finalspecular;
+        vec3 ambdiff = texcolor.rgb * v_Color * ( finalambient + finaldiffuse );
+        vec3 spec = u_TextureSpecColor.rgb * finalspecular;
 
         // Calculate final color including whether it's in shadow or not.
-        gl_FragColor = ( ambdiff + spec ) * shadowperc;
-        gl_FragColor.a = 1.0;
+        gl_FragColor.rgb = ( ambdiff + spec ) * shadowperc;
+        gl_FragColor.a = texcolor.a;
     }
 
 #endif //Fragment Shader
