83fff257e895e17b63d657888505ba7bf6c95786
Jimmy Lord <jimmylord@gmail.com>
2017-12-14 07:11:53 -0500

    Closing window on OSX now closes app.

--- GameEmptyReplaceMe/Game/SourceOSX/AppDelegate.mm
+++ GameEmptyReplaceMe/Game/SourceOSX/AppDelegate.mm
@@ -46,6 +46,11 @@
 {
 }
 
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender
+{
+    return YES;
+}
+
 - (void)applicationWillResignActive:(NSNotification *)notification
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
