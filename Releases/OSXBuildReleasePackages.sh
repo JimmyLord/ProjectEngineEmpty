f8a35f6db4696eb2fd77ab564654866bd68f7f88
Jimmy Lord <jimmylord@gmail.com>
2017-12-18 21:07:30 -0500

    Updated iOS project and build scripts.

--- Releases/OSXBuildReleasePackages.sh
+++ Releases/OSXBuildReleasePackages.sh
@@ -1,4 +1,9 @@
-cd ../GameEmptyReplaceMe
+pushd ../GameEmptyReplaceMe
+
+echo ""
+echo "$(tput setaf 5)==> Building Debug Package: iOS$(tput sgr0)"
+xcodebuild -scheme EmptyReplaceMe-iOS -config Release build -sdk iphonesimulator CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+xcodebuild -scheme EmptyReplaceMe-iOS -config Release build -sdk iphoneos CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
 
 echo ""
 echo "$(tput setaf 5)==> Building Release Package: Game$(tput sgr0)"
@@ -7,3 +12,5 @@ xcodebuild -scheme EmptyReplaceMe-OSX -config Release build
 echo ""
 echo "$(tput setaf 5)==> Building Release Package: Editor$(tput sgr0)"
 xcodebuild -scheme EmptyReplaceMe-wx -config Release build
+
+popd
--- Releases/OSXCopyReleasePackages.sh
+++ Releases/OSXCopyReleasePackages.sh
@@ -1,6 +1,11 @@
 if [ "$1" != "" ]; then
 
     echo ""
+    echo "$(tput setaf 5)==> Copying iOS Unsigned Release Package$(tput sgr0)"
+    rsync -avL ../GameEmptyReplaceMe/Output/EmptyReplaceMe/Build/Products/Release-iphonesimulator/EmptyReplaceMe-iOS.app/ $1/MyEngine_Game_iOS_Simulator.app/
+    rsync -avL ../GameEmptyReplaceMe/Output/EmptyReplaceMe/Build/Products/Release-iphoneos/EmptyReplaceMe-iOS.app/ $1/MyEngine_Game_iOS_Device.app/
+
+    echo ""
     echo "$(tput setaf 5)==> Copying Release Package: Game$(tput sgr0)"
     rsync -avL ../GameEmptyReplaceMe/Output/EmptyReplaceMe/Build/Products/Release/EmptyReplaceMe-OSX.app/ $1/MyEngine_Game.app/
 
