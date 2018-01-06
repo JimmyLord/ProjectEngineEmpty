af6ea3efc72a835aaae911368666481e1f07321e
Jimmy Lord <jimmylord@gmail.com>
2018-01-06 08:16:18 -0500

    Updated Engine, Framework and some scripts.

--- Releases/OSXBuildReleasePackagesAndCopy_iOS6.sh
+++ Releases/OSXBuildReleasePackagesAndCopy_iOS6.sh
@@ -2,12 +2,16 @@ if [ "$1" != "" ]; then
 
     pushd ../GameEmptyReplaceMe
 
+    echo ""
+    echo "$(tput setaf 5)==> Switching to Xcode 7.3.1$(tput sgr0)"
     sudo xcode-select -switch /Applications/Xcode731/Xcode.app
 
     echo ""
     echo "$(tput setaf 5)==> Building Release Package for iOS6: iOS$(tput sgr0)"
     xcodebuild -xcconfig ../Releases/XcodeConfig-iOS6.xcconfig -scheme EmptyReplaceMe-iOS -config Release build -sdk iphoneos CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
 
+    echo ""
+    echo "$(tput setaf 5)==> Switching back to latest Xcode$(tput sgr0)"
     sudo xcode-select -switch /Applications/Xcode92/Xcode.app
 
     popd
--- Releases/iOS/BuildAndLaunch-DeviceiOS6.sh
+++ Releases/iOS/BuildAndLaunch-DeviceiOS6.sh
@@ -3,6 +3,8 @@ source Setup.sh
 
 cd ..
 
+echo ""
+echo "$(tput setaf 5)==> Switching to Xcode 7.3.1$(tput sgr0)"
 sudo xcode-select -switch /Applications/Xcode731/Xcode.app
 
 echo ""
@@ -17,4 +19,6 @@ echo ""
 echo "$(tput setaf 5)==> Launching app$(tput sgr0)"
 ios-deploy --justlaunch --bundle MyEngine_Game_iOS6_Device.app/
 
+echo ""
+echo "$(tput setaf 5)==> Switching back to latest Xcode$(tput sgr0)"
 sudo xcode-select -switch /Applications/Xcode92/Xcode.app
